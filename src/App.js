76a5a230b184d45f9ea9dc4c82bc89ba8b8b835a
MuditJain2804 <muditjain2804@gmail.com>
2023-11-16 10:44:05 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -113,10 +113,198 @@ import './App.css';
 //   return <Greeting/>
 // }
 
-function Greeting({ name }) {
-  return <h1>Hello, {name}</h1>;
-}
+// function Greeting({ name }) {
+//   return <h1>Hello, {name}</h1>;
+// }
 
-export default function App() {
-  return <Greeting name="world" />
+// export default function App() {
+//   return <Greeting name="world" />
+// }
+
+// export default function Avatar() {
+//   const avatar = 'https://i.imgur.com/7vQD0fPs.jpg';
+//   const description = 'Gregorio Y. Zara';
+//   return (
+//     <img
+//       className="avatar"
+//       src={avatar}
+//       alt={description}
+//     />
+//   );
+// }
+
+// const baseUrl = 'https://i.imgur.com/';
+// const person = {
+//   name: 'Gregorio Y. Zara',
+//   imageId: '7vQD0fP',
+//   imageSize: 's',
+//   theme: {
+//     backgroundColor: 'black',
+//     color: 'pink'
+//   }
+// };
+
+// export default function TodoList() {
+//   return (
+//     <div style={person.theme}>
+//       <h1>{person.name}'s Todos</h1>
+//       <img
+//         className="avatar"
+//         src={baseUrl + person.imageId + person.imageSize + '.jpg'}
+//         alt={person.name}
+//       />
+//       <ul>
+//         <li>Improve the videophone</li>
+//         <li>Prepare aeronautics lectures</li>
+//         <li>Work on the alcohol-fuelled engine</li>
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export function getImageUrl(imageId, size = 's') {
+//   return (
+//     'https://i.imgur.com/' +
+//     imageId +
+//     size +
+//     '.jpg'
+//   );
+// }
+
+// function Profile({imageId,name, size=70,profession,award,discovery}){
+//   return (
+//       <section className="profile">
+//         <h2>{name}</h2>
+//       <img
+//         className="avatar"
+//         src = {getImageUrl(imageId)}
+//         alt={name}
+//         width={size}
+//         height={size}
+//         />
+//         <ul>
+//           <li>
+//             <b>Profession: </b> 
+//             {profession}
+//           </li>
+//           <li>
+//             <b>Awards: {award.length}</b> 
+//             ({award.join(',')})
+//           </li>
+//           <li>
+//             <b>Discovered: </b>
+//             {discovery}
+//           </li>
+//         </ul>
+//       </section>
+//   )
+// }
+
+// export default function Gallery() {
+//   return (
+//     <div>
+//       <h1>Notable Scientists</h1>
+//         <Profile
+//           imageId = "szV5sdG"
+//           name="Maria Skłodowska-Curie"
+//           discovery="polonium (chemical element)"
+//           profession="physicist and chemist"
+//           award={['Nobel Prize in Physics','Nobel Prize in Chemistry','Davy Medal','Matteucci Medal']}
+//         />
+//         <Profile
+//           imageId = "YfeOqp2"
+//           name="Katsuko Saruhashi"
+//           discovery="a method for measuring carbon dioxide in seawater"
+//           profession="physicist and chemist"
+//           award={['Miyake Prize for geochemistry','Tanaka Prize']}
+//         />
+//         </div>
+//   );
+// }
+
+
+// export default function app(){
+//   return(
+//     <button>
+//       Click me
+//     </button>
+//   )
+// }
+
+// export default function app(){
+//   function handleClick(){
+//     alert('You clicked me !')
+//   }
+
+//   return(
+//     <button onClick={handleClick}>
+//       Click me
+//     </button>
+//   )
+// }
+
+// export default function Signup() {
+//   return (
+//     <form onSubmit={() => alert('Submitting!')}>
+//       <input />
+//       <button>Send</button>
+//     </form>
+//   );
+// }
+
+import {useState} from 'react';
+import { sculptureList } from './data';
+
+export default function Gallery(){
+    const[index,setIndex] = useState(0);
+    const[showMore, setShowMore] = useState(false);
+
+    let hasPrev = index>0;
+    let hasNext = index<sculptureList.length - 1;
+
+    function handleNextClick(){
+        if(hasNext){
+            setIndex(index + 1);
+        }
+    }
+    function handlePrevClick(){
+        if(hasPrev){
+            setIndex(index -1);
+        }
+    }
+    function handleMoreClick(){
+        setShowMore(!showMore);
+    }
+
+    let sculpture = sculptureList[index];
+
+    return(
+        <>
+            <button 
+                onClick={handlePrevClick}
+                disabled = {!hasPrev}
+            >
+                Previous
+            </button>
+            <button 
+                onClick={handleNextClick}
+                disabled = {!hasNext}
+            >
+                Next
+            </button>
+            <h2><i>{sculpture.name}</i> by {sculpture.artist}</h2>
+            <h3>({index + 1 } of {sculptureList.length})</h3>
+            <button onClick={handleMoreClick}>
+                {showMore ? 'Hide' : 'Show'} details
+            </button>
+            <p>
+            {showMore && sculpture.description}
+            </p>
+            <img 
+                src={sculpture.url}
+                alt={sculpture.alt}
+            />
+        </>
+    );
+
 }
